bc4a868fb94c84132627aa7a23e308878e84be52
athenexfolk <folkadpocket99@gmail.com>
2023-11-21 23:44:59 +0700

    add image route guard

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -5,25 +5,30 @@ import {
   UploadedFile,
   Get,
   Param,
+  UseGuards,
 } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from 'src/auth/guards/auth/auth.guard';
 
 @Controller('api/images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
+  @UseGuards(AuthGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.imagesService.create(file);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   getImagesPath() {
     return this.imagesService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   getImagePath(@Param('id') id: string) {
     return this.imagesService.findById(id);
