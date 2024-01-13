d500c4ff521d1ed9043c4af06792acf812b397e6
athenexfolk <folkadpocket99@gmail.com>
2024-01-14 02:38:49 +0700

    add upload file endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LessonModule } from './lesson/lesson.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/nerdoverdb'),
-    LessonModule
+    LessonModule,
+    MulterModule.register({
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, join(__dirname, '../public/upload'));
+        },
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, `${file.fieldname}-${uniqueSuffix}-${file.originalname}`);
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
